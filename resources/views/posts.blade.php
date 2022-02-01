7828ac3aef886e8a66f7ede12bfe6494c98f9f73
SpyderOrb <alex.plachinda@gmail.com>
2022-02-01 18:02:17 +0100

    first push

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style.css">
+    <title>My blog</title>
+</head>
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">First article</a></h1>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus at nam veritatis, dolorum quos eaque harum voluptatibus est delectus similique ab perspiciatis reprehenderit porro facere voluptate, officia, incidunt illo aliquid.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">Second article</a></h1>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus at nam veritatis, dolorum quos eaque harum voluptatibus est delectus similique ab perspiciatis reprehenderit porro facere voluptate, officia, incidunt illo aliquid.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">Third article</a></h1>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus at nam veritatis, dolorum quos eaque harum voluptatibus est delectus similique ab perspiciatis reprehenderit porro facere voluptate, officia, incidunt illo aliquid.
+        </p>
+    </article>
+</body>
+
+</html>
