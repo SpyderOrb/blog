547834a528accd9568fb9d5760dbd34421026473
SpyderOrb <alex.plachinda@gmail.com>
2022-02-02 12:43:59 +0100

    Added collections and updated Post class

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use PhpParser\Node\Expr\PostDec;
 use App\Models\Post;
 
 /*
@@ -16,14 +15,53 @@ use App\Models\Post;
 */
 
 Route::get('/', function () {
+    //    $posts = [];
+    //
+    //    foreach ($files as $file) {
+    //        $document = YamlFrontMatter::parseFile($file);
+    //        $posts[]  = new Post(
+    //            $document->title,
+    //            $document->excerpt,
+    //            $document->date,
+    //            $document->body(),
+    //            $document->slug,
+    //        );
+    //    }
+    
+    // Using array_map()
+    
+    //    $posts = array_map(function ($file) {
+    //        $document = YamlFrontMatter::parseFile($file);
+    //        return new Post(
+    //            $document->title,
+    //            $document->date,
+    //            $document->excerpt,
+    //            $document->body(),
+    //            $document->slug,
+    //        );
+    //    }, $files);
+    
+    // Using collections
+    
+    //    $posts = collect(File::files(resource_path("posts")))
+    //        ->map(fn($file) => YamlFrontMatter::parseFile($file))
+    //        ->map(fn($document) => new Post(
+    //            $document->title,
+    //            $document->date,
+    //            $document->excerpt,
+    //            $document->body(),
+    //            $document->slug,
+    //        ));
+    
     return view('posts', [
         'posts' => Post::all()
     ]);
 });
 
 Route::get('posts/{post}', function ($slug) {
-
     // Find a post by its slug and pass it to a view called "post"
-    return view('post', ['post' => Post::find($slug)]);
-
-})->where('post', '[A-Za-z_\-]+');
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
+})
+    ->where('post', '[A-Za-z_\-]+');
